418b038688e0f880e37ee9aa4150fc983b117ce8
Michał Terczyński <mterczynski1@gmail.com>
2022-11-20 00:07:46 +0100

    Write solve3x3Squares test

--- src/solvers.test.ts
+++ src/solvers.test.ts
@@ -1,5 +1,11 @@
-import { solveRows } from "./solvers"
+import { solve3x3Squares, solveRows } from "./solvers"
 import { createEmptyDetailedBoard } from "./testUtils"
+import type { DetailedTile } from "./types"
+
+function setTileValue(tile: DetailedTile, value: number) {
+  tile.value = value
+  tile.possibleValues = []
+}
 
 describe('solvers', () => {
   describe('solveRows', () => {
@@ -7,8 +13,7 @@ describe('solvers', () => {
       const mockDetailedBoard = createEmptyDetailedBoard()
 
       for (let i = 0; i < 8; i++) {
-        mockDetailedBoard[0][i].value = i + 1
-        mockDetailedBoard[0][i].possibleValues = []
+        setTileValue(mockDetailedBoard[0][i], i + 1)
       }
 
       const checkResult = solveRows(mockDetailedBoard)
@@ -18,7 +23,33 @@ describe('solvers', () => {
       expect(mockDetailedBoard[0][8].possibleValues).toEqual([])
       // check if previously filled values are left untouched:
       expect(mockDetailedBoard[0][0].value).toEqual(1)
-      expect(mockDetailedBoard[0][1].value).toEqual(2)
+      expect(mockDetailedBoard[0][0].possibleValues).toEqual([])
+    })
+  })
+
+  describe('solve3x3Squares', () => {
+    it('solves 3x3 Squares', () => {
+      const mockDetailedBoard = createEmptyDetailedBoard()
+
+      setTileValue(mockDetailedBoard[0][0], 1)
+      setTileValue(mockDetailedBoard[0][1], 2)
+      setTileValue(mockDetailedBoard[0][2], 3)
+
+      setTileValue(mockDetailedBoard[1][0], 4)
+      setTileValue(mockDetailedBoard[1][1], 5)
+      setTileValue(mockDetailedBoard[1][2], 6)
+
+      setTileValue(mockDetailedBoard[2][0], 7)
+      setTileValue(mockDetailedBoard[2][1], 8)
+
+      const checkResult = solve3x3Squares(mockDetailedBoard)
+
+      expect(checkResult).toEqual(true)
+      expect(mockDetailedBoard[2][2].value).toEqual(9)
+      expect(mockDetailedBoard[2][2].possibleValues).toEqual([])
+      // check if previously filled values are left untouched:
+      expect(mockDetailedBoard[0][0].value).toEqual(1)
+      expect(mockDetailedBoard[0][0].possibleValues).toEqual([])
     })
   })
 })
