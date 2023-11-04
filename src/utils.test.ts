32614024744339ef3f31f6a7aaf3f440aada2e9e
Michał Terczyński <mterczynski1@gmail.com>
2023-11-04 19:46:45 +0100

    Add getSquareOfTile test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -19,17 +19,25 @@ describe('utils', () => {
   });
 
   describe('getSquareOfTile', () => {
-    it('returns a 3x3 square to which the provided tile belongs to', () => {
-      const board = createEasySudoku();
-      const mockDetailedBoard: DetailedBoard = board.map((row) =>
-        row.map((tileValue) => ({
-          value: tileValue,
-          possibleValues: [], // not relevant to this scenario
-        })),
-      );
-
+    const board = createEasySudoku();
+    const mockDetailedBoard: DetailedBoard = board.map((row) =>
+      row.map((tileValue) => ({
+        value: tileValue,
+        possibleValues: [], // not relevant to this scenario
+      })),
+    );
+    it('returns a 3x3 square to which the provided tile belongs to - top right corner', () => {
       const columnIndex = 5;
       const rowIndex = 3;
+
+      const result = getSquareOfTile(mockDetailedBoard, columnIndex, rowIndex);
+
+      expect(result.map((i) => i.value)).toEqual([0, 0, 0, 6, 2, 0, 9, 0, 1]);
+    });
+
+    it('returns a 3x3 square to which the provided tile belongs to - bottom left corner', () => {
+      const columnIndex = 3;
+      const rowIndex = 5;
 
       const result = getSquareOfTile(mockDetailedBoard, columnIndex, rowIndex);
 
